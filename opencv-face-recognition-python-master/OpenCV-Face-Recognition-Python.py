a9f6c5008d035be08e74490c1f57fee470b6f708
Anish Krishnan <anishtech1@gmail.com>
2019-02-02 20:08:12 -0500

    did not add videoStream yet

--- opencv-face-recognition-python-master/OpenCV-Face-Recognition-Python.py
+++ opencv-face-recognition-python-master/OpenCV-Face-Recognition-Python.py
@@ -98,6 +98,24 @@ def detect_face(img):
     #return only the face part of the image
     return gray[y:y+w, x:x+h], faces[0]
 
+def detect_car(img):
+    #convert the test image to gray image as opencv face detector expects gray images
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #load OpenCV face detector, I am using LBP which is fast
+    #there is also a more accurate but slow Haar classifier
+    face_cascade = cv2.CascadeClassifier('opencv-files/cars.xml')
+    #let's detect multiscale (some images may be closer to camera than others) images
+    #result is a list of faces
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
+    #if no faces are detected then return original img
+    if (len(faces) == 0):
+        return None, None
+    #under the assumption that there will be only one face,
+    #extract the face area
+    (x, y, w, h) = faces[0]
+    #return only the face part of the image
+    return gray[y:y+w, x:x+h], faces[0]
+
 
 # I am using OpenCV's **LBP face detector**. On _line 4_, I convert the image to grayscale because most operations in OpenCV are performed in gray scale, then on _line 8_ I load LBP face detector using `cv2.CascadeClassifier` class. After that on _line 12_ I use `cv2.CascadeClassifier` class' `detectMultiScale` method to detect all the faces in the image. on _line 20_, from detected faces I only pick the first face because in one image there will be only one face (under the assumption that there will be only one prominent face). As faces returned by `detectMultiScale` method are actually rectangles (x, y, width, height) and not actual faces images so we have to extract face image area from the main image. So on _line 23_ I extract face area from gray image and return both the face image area and face rectangle.
 #
@@ -139,22 +157,18 @@ def prepare_training_data(data_folder_path):
 
     #let's go through each directory and read images within it
     for dir_name in dirs:
-
         #our subject directories start with letter 's' so
         #ignore any non-relevant directories if any
         if not dir_name.startswith("s"):
             continue;
-
         #------STEP-2--------
         #extract label number of subject from dir_name
         #format of dir name = slabel
         #, so removing letter 's' from dir_name will give us label
         label = int(dir_name.replace("s", ""))
-
         #build path of directory containin images for current subject subject
         #sample subject_dir_path = "training-data/s1"
         subject_dir_path = data_folder_path + "/" + dir_name
-
         #get the images names that are inside the given subject directory
         subject_images_names = os.listdir(subject_dir_path)
 
@@ -177,9 +191,9 @@ def prepare_training_data(data_folder_path):
             #display an image window to show the image
             cv2.imshow("Training on image...", cv2.resize(image, (400, 500)))
             cv2.waitKey(1)
-
             #detect face
             face, rect = detect_face(image)
+            car, rect = detect_car(image)
 
             #------STEP-4--------
             #for the purpose of this tutorial
