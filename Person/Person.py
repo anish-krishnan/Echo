b39a0d995f6dd970df7034971733a670afb832b6
userv562 <vashisth.parekh@gmail.com>
2019-02-02 21:41:26 -0500

    _

--- Person/Person.py
+++ Person/Person.py
@@ -6,7 +6,7 @@ class Person:
     def __init__(self, name):
         self.name = name
 
-        self.training_data_path_for_person = "/Users/vashisth/PycharmProjects/PennApps2018/FacesDatabase/"+name
+        self.training_data_path_for_person = "/.../FacesDatabase/"+name
         if not os.path.exists(self.training_data_path_for_person):
             os.makedirs(self.training_data_path_for_person)
 
